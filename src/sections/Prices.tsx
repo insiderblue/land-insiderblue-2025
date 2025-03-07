0fc74b936d7a89272c23b90f7b9f43fdc762d624
Gustavo <whoisgustavolima@gmail.com>
2025-03-07 18:21:23 -0300

    insiderblue

--- src/sections/Prices.tsx
+++ src/sections/Prices.tsx
@@ -10,6 +10,52 @@ import { Badge } from '@/components/Badge';
 import { Button } from '@/components/Button';
 
 export default function Prices() {
+
+    const whatsapp = '5535984365367';
+
+    const features = [
+        'Site totalmente responsivo',
+        'Banner com frase de efeito',
+        'Botão flutuante para o WhatsApp',
+        'Seção de sobre',
+        'Seção de produtos/serviços/preços',
+        'Avaliações do Google',
+        'Formulário de contato'
+    ];
+
+    const cards = [
+        {
+            icon: '/img/icon-4.png',
+            title: 'START',
+            description: 'Para quem está começando',
+            features: [0,1,2,3],
+            price: '690,00',
+            subscription: '+ 54,90 mensal',
+            button: 'Quero um site START',
+            message: 'Olá. Gostaria de um site Start!',
+        },
+        {
+            icon: '/img/icon-5.png',
+            title: 'PLUS',
+            description: 'Para negócios em crescimento',
+            features: [0,1,2,3,4],
+            price: '890,00',
+            subscription: '+ 64,90 mensal',
+            button: 'Quero um site PLUS',
+            message: 'Olá. Gostaria de um site Plus!',
+        },
+        {
+            icon: '/img/icon-6.png',
+            title: 'MASTER',
+            description: 'Para marcas consolidadas',
+            features: [0,1,2,3,4,5,6],
+            price: '1490,00',
+            subscription: '+ 74,90 mensal',
+            button: 'Quero um site MASTER',
+            message: 'Olá. Gostaria de um site Master!',
+        },
+    ];
+
     return (
         <Section id="prices" variant="even">
             <Container className="flex flex-col items-center py-28 pb-34">
@@ -17,144 +63,32 @@ export default function Prices() {
                 <Badge>Nossos planos</Badge>
                 </div>
                 <div className="flex gap-5">
-                    <Card>
-                        <Image
-                        src="/img/icon-1.png"
-                        alt="Icone"
-                        width={83}
-                        height={100}
-                        className="mb-16"
-                        />
-                        <h5 className="mb-4">START</h5>
-                        <p className="mb-9">Para quem está começando</p>
-                        <ul className="text-left flex flex-col gap-2 mb-10">
-                        <li className="flex items-center gap-3">
-                            <FontAwesomeIcon icon={faCheck} className="text-accent w-4" />
-                            <span>Site totalmente responsivo</span>
-                        </li>
-                        <li className="flex items-center gap-3">
-                            <FontAwesomeIcon icon={faCheck} className="text-accent w-4" />
-                            <span>Banner com frase de efeito</span>
-                        </li>
-                        <li className="flex items-center gap-3">
-                            <FontAwesomeIcon icon={faCheck} className="text-accent w-4" />
-                            <span>Botão flutuante para o WhatsApp</span>
-                        </li>
-                        <li className="flex items-center gap-3">
-                            <FontAwesomeIcon icon={faCheck} className="text-accent w-4" />
-                            <span>Seção de sobre</span>
-                        </li>
-                        <li className="flex items-center gap-3 opacity-50 line-through">
-                            <FontAwesomeIcon icon={faCheck} className="text-accent w-4" />
-                            <span>Seção de produtos/serviços/preços</span>
-                        </li>
-                        <li className="flex items-center gap-3 opacity-50 line-through">
-                            <FontAwesomeIcon icon={faCheck} className="text-accent w-4" />
-                            <span>Avaliações do Google</span>
-                        </li>
-                        <li className="flex items-center gap-3 opacity-50 line-through">
-                            <FontAwesomeIcon icon={faCheck} className="text-accent w-4" />
-                            <span>Formulário de contato</span>
-                        </li>
-                        </ul>
-                        <div className="mb-10">
-                        <h5 className="mb-2">690,00<span className="text-accent">*</span></h5>
-                        <p>+ 54,90 mensal</p>
-                        </div>
-                        <Button href="https://web.whatsapp.com/send?phone=5535984365367&text=Olá. Gostaria de um site Start!" target="_blank">Quero um site Start</Button>
-                    </Card>
-                    <Card>
-                        <Image
-                        src="/img/icon-5.png"
-                        alt="Icone"
-                        width={100}
-                        height={100}
-                        className="mb-16"
-                        />
-                        <h5 className="mb-2">PLUS</h5>
-                        <p className="mb-9">Para negócios em crescimento</p>
-                        <ul className="text-left flex flex-col gap-2 mb-10">
-                        <li className="flex items-center gap-3">
-                            <FontAwesomeIcon icon={faCheck} className="text-accent w-4" />
-                            <span>Site totalmente responsivo</span>
-                        </li>
-                        <li className="flex items-center gap-3">
-                            <FontAwesomeIcon icon={faCheck} className="text-accent w-4" />
-                            <span>Banner com frase de efeito</span>
-                        </li>
-                        <li className="flex items-center gap-3">
-                            <FontAwesomeIcon icon={faCheck} className="text-accent w-4" />
-                            <span>Botão flutuante para o WhatsApp</span>
-                        </li>
-                        <li className="flex items-center gap-3">
-                            <FontAwesomeIcon icon={faCheck} className="text-accent w-4" />
-                            <span>Seção de sobre</span>
-                        </li>
-                        <li className="flex items-center gap-3">
-                            <FontAwesomeIcon icon={faCheck} className="text-accent w-4" />
-                            <span>Seção de produtos/serviços/preços</span>
-                        </li>
-                        <li className="flex items-center gap-3 opacity-50 line-through">
-                            <FontAwesomeIcon icon={faCheck} className="text-accent w-4" />
-                            <span>Avaliações do Google</span>
-                        </li>
-                        <li className="flex items-center gap-3 opacity-50 line-through">
-                            <FontAwesomeIcon icon={faCheck} className="text-accent w-4" />
-                            <span>Formulário de contato</span>
-                        </li>
-                        </ul>
-                        <div className="mb-10">
-                        <h5 className="mb-2">890,00<span className="text-accent">*</span></h5>
-                        <p>+ 64,90 mensal</p>
-                        </div>
-                        <Button href="https://web.whatsapp.com/send?phone=5535984365367&text=Olá. Gostaria de um site Plus!" target="_blank">Quero um site Plus</Button>
-                    </Card>
-                    <Card>
-                        <Image
-                        src="/img/icon-6.png"
-                        alt="Icone"
-                        width={100}
-                        height={100}
-                        className="mb-16"
-                        />
-                        <h5 className="mb-2">MASTER</h5>
-                        <p className="mb-9">Para marcas consolidadas</p>
-                        <ul className="text-left flex flex-col gap-2 mb-10">
-                        <li className="flex items-center gap-3">
-                            <FontAwesomeIcon icon={faCheck} className="text-accent w-4" />
-                            <span>Site totalmente responsivo</span>
-                        </li>
-                        <li className="flex items-center gap-3">
-                            <FontAwesomeIcon icon={faCheck} className="text-accent w-4" />
-                            <span>Banner com frase de efeito</span>
-                        </li>
-                        <li className="flex items-center gap-3">
-                            <FontAwesomeIcon icon={faCheck} className="text-accent w-4" />
-                            <span>Botão flutuante para o WhatsApp</span>
-                        </li>
-                        <li className="flex items-center gap-3">
-                            <FontAwesomeIcon icon={faCheck} className="text-accent w-4" />
-                            <span>Seção de sobre</span>
-                        </li>
-                        <li className="flex items-center gap-3">
-                            <FontAwesomeIcon icon={faCheck} className="text-accent w-4" />
-                            <span>Seção de produtos/serviços/preços</span>
-                        </li>
-                        <li className="flex items-center gap-3">
-                            <FontAwesomeIcon icon={faCheck} className="text-accent w-4" />
-                            <span>Avaliações do Google</span>
-                        </li>
-                        <li className="flex items-center gap-3">
-                            <FontAwesomeIcon icon={faCheck} className="text-accent w-4" />
-                            <span>Formulário de contato</span>
-                        </li>
-                        </ul>
-                        <div className="mb-10">
-                        <h5 className="mb-2">1490,00<span className="text-accent">*</span></h5>
-                        <p>+ 74,90 mensal</p>
-                        </div>
-                        <Button href="https://web.whatsapp.com/send?phone=5535984365367&text=Olá. Gostaria de um site Master!" target="_blank">Quero um site Master</Button>
-                    </Card>
+                    {cards.map((card,i) => (
+                        <Card key={i}>
+                            <Image
+                            src={card.icon}
+                            alt=""
+                            width={100}
+                            height={100}
+                            className="mb-16"
+                            />
+                            <h5 className="mb-2">{card.title}</h5>
+                            <p className="mb-9">{card.description}</p>
+                            <ul className="text-left flex flex-col gap-2 mb-10">
+                                {features.map((feature,j) => (
+                                    <li key={j} className={`flex items-center gap-3 ${!card.features.includes(j)?'opacity-50 line-through':''}`}>
+                                        <FontAwesomeIcon icon={faCheck} className="text-accent w-4" />
+                                        <span>{feature}</span>
+                                    </li>
+                                ))}
+                            </ul>
+                            <div className="mb-10">
+                            <h5 className="mb-2">{card.price}<span className="text-accent">*</span></h5>
+                            <p>{card.subscription}</p>
+                            </div>
+                            <Button href={`https://web.whatsapp.com/send?phone=${whatsapp}&text=${card.message}`} target="_blank">{card.button}</Button>
+                        </Card>
+                    ))}
                 </div>
                 <div className="mt-14 text-center max-w-[600px]">
                     * O valor referente ao desenvolvimento pode ser pago em duas parcelas, sendo a primeira anteriormente ao inicio do projeto e a segunda durante a entrega. 
