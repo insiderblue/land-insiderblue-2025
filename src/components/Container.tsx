9ad84bb5f5ab2643742ac146825c641075fb7e1c
Gustavo <whoisgustavolima@gmail.com>
2025-03-07 08:55:22 -0300

    insiderblue

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 export function Container({children, className}:Props) {
     return (
-        <div className={`max-w-full h-full mx-4 xl:w-[1290px] xl:mx-auto ${className?className:``}`}>
+        <div className={`w-full h-full mx-4 px-24 xl:max-w-[1290px] xl:mx-auto ${className?className:``}`}>
             {children}
         </div>
     )
