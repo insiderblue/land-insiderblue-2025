4ddc8a7f321ce0f6e45c9213efc92c63fa8e3a36
Gustavo <whoisgustavolima@gmail.com>
2025-03-06 22:03:10 -0300

    insiderblue

--- src/sections/Clients.tsx
+++ src/sections/Clients.tsx
@@ -7,80 +7,80 @@ import { Badge } from '@/components/Badge';
 
 export default function Hero() {
     return (
-        <Section variant="odd">
-        <Divisor variant={2}/>
-        <Container className="flex flex-col items-center pt-28 pb-42">
-          <div className="mb-14">
-            <Badge>Nossos clientes</Badge>
-          </div>
-          <div className="w-full flex flex-col xl:flex-row gap-8">
-            <div className="w-[491px] max-w-full"> 
-              <h1 className="bg-gradient-to-r from-accent to-dark-accent bg-clip-text text-transparent uppercase">Marcas que confiam no nosso trabalho e estão usando sites Insiderblue</h1>
+      <Section id="clients" variant="odd">
+          <Divisor variant={2}/>
+          <Container className="flex flex-col items-center pt-28 pb-42">
+            <div className="mb-14">
+              <Badge>Nossos clientes</Badge>
             </div>
-            <div className="flex flex-wrap items-center justify-center gap-4 flex-1">
-              <Image
-                src="/img/logo-1.png"
-                alt="Logo"
-                width={139}
-                height={139}
-              />
-              <Image
-                src="/img/logo-2.png"
-                alt="Logo"
-                width={139}
-                height={139}
-              />
-              <Image
-                src="/img/logo-3.png"
-                alt="Logo"
-                width={139}
-                height={139}
-              />
-              <Image
-                src="/img/logo-4.png"
-                alt="Logo"
-                width={139}
-                height={139}
-              />
-              <Image
-                src="/img/logo-5.png"
-                alt="Logo"
-                width={139}
-                height={139}
-              />
-              <Image
-                src="/img/logo-6.png"
-                alt="Logo"
-                width={139}
-                height={139}
-              />
-              <Image
-                src="/img/logo-7.png"
-                alt="Logo"
-                width={139}
-                height={139}
-              />
-              <Image
-                src="/img/logo-8.png"
-                alt="Logo"
-                width={139}
-                height={139}
-              />
-              <Image
-                src="/img/logo-9.png"
-                alt="Logo"
-                width={139}
-                height={139}
-              />
-              <Image
-                src="/img/logo-10.png"
-                alt="Logo"
-                width={139}
-                height={139}
-              />
+            <div className="w-full flex flex-col xl:flex-row gap-8">
+              <div className="w-[491px] max-w-full"> 
+                <h1 className="bg-gradient-to-r from-accent to-dark-accent bg-clip-text text-transparent uppercase">Marcas que confiam no nosso trabalho e estão usando sites Insiderblue</h1>
+              </div>
+              <div className="flex flex-wrap items-center justify-center gap-4 flex-1">
+                <Image
+                  src="/img/logo-1.png"
+                  alt="Logo"
+                  width={139}
+                  height={139}
+                />
+                <Image
+                  src="/img/logo-2.png"
+                  alt="Logo"
+                  width={139}
+                  height={139}
+                />
+                <Image
+                  src="/img/logo-3.png"
+                  alt="Logo"
+                  width={139}
+                  height={139}
+                />
+                <Image
+                  src="/img/logo-4.png"
+                  alt="Logo"
+                  width={139}
+                  height={139}
+                />
+                <Image
+                  src="/img/logo-5.png"
+                  alt="Logo"
+                  width={139}
+                  height={139}
+                />
+                <Image
+                  src="/img/logo-6.png"
+                  alt="Logo"
+                  width={139}
+                  height={139}
+                />
+                <Image
+                  src="/img/logo-7.png"
+                  alt="Logo"
+                  width={139}
+                  height={139}
+                />
+                <Image
+                  src="/img/logo-8.png"
+                  alt="Logo"
+                  width={139}
+                  height={139}
+                />
+                <Image
+                  src="/img/logo-9.png"
+                  alt="Logo"
+                  width={139}
+                  height={139}
+                />
+                <Image
+                  src="/img/logo-10.png"
+                  alt="Logo"
+                  width={139}
+                  height={139}
+                />
+              </div>
             </div>
-          </div>
-        </Container>
+          </Container>
       </Section>
     )
 }
