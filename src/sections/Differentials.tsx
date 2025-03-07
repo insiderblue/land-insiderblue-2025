8ef3068f2df54fb4d99b60b4e8cbff0a9cbdb271
Gustavo <whoisgustavolima@gmail.com>
2025-03-07 18:08:28 -0300

    insiderblue

--- src/sections/Differentials.tsx
+++ src/sections/Differentials.tsx
@@ -7,6 +7,25 @@ import { Card } from '@/components/Card';
 import { Badge } from '@/components/Badge';
 
 export default function Differentials() {
+
+    const cards = [
+        {
+            icon: '/img/icon-1.png',
+            title: 'Sites gerenciáveis',
+            description: 'Você pode alterar o conteúdo do seu site sempre que quiser.'
+        },
+        {
+            icon: '/img/icon-2.png',
+            title: 'Sites responsivos',
+            description: 'Nossos sites são compatíveis com celulares e computadores.'
+        },
+        {
+            icon: '/img/icon-3.png',
+            title: 'Sites otimizados',
+            description: 'Nossos sites são leves e não apresentam qualquer tipo de lentidão.'
+        }
+    ];
+
     return (
         <Section id="differentials" variant="even">
             <Divisor variant={1}/>
@@ -15,39 +34,19 @@ export default function Differentials() {
                 <Badge>Estrutura Completa</Badge>
                 </div>
                 <div className="flex flex-col xl:flex-row gap-5">
-                <Card>
-                    <Image
-                    src="/img/icon-1.png"
-                    alt="Icone"
-                    width={80}
-                    height={80}
-                    className="mb-16"
-                    />
-                    <h5 className="mb-3 uppercase">Sites gerenciáveis</h5>
-                    <p>Você pode alterar o conteúdo do seu site sempre que quiser.</p>
-                </Card>
-                <Card>
-                    <Image
-                    src="/img/icon-2.png"
-                    alt="Icone"
-                    width={80}
-                    height={80}
-                    className="mb-16"
-                    />
-                    <h5 className="mb-3 uppercase">Sites responsivos</h5>
-                    <p>Nossos sites são compatíveis com celulares e computadores.</p>
-                </Card>
-                <Card>
-                    <Image
-                    src="/img/icon-3.png"
-                    alt="Icone"
-                    width={80}
-                    height={80}
-                    className="mb-16"
-                    />
-                    <h5 className="mb-3 uppercase">Sites otimizados</h5>
-                    <p>Nossos sites são leves e não apresentam qualquer tipo de lentidão.</p>
-                </Card>
+                    {cards.map((card,index) => (
+                        <Card key={index}>
+                            <Image
+                            src={card.icon}
+                            alt=""
+                            width={80}
+                            height={80}
+                            className="mb-16"
+                            />
+                            <h5 className="mb-3 uppercase">{card.title}</h5>
+                            <p>{card.description}</p>
+                        </Card>
+                    ))}
                 </div>
             </Container>
         </Section>
