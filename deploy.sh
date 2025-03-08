8ae01d718aa9bd457868e84a50ef61f715b94e00
Gustavo <whoisgustavolima@gmail.com>
2025-03-08 00:06:27 -0300

    insiderblue

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
+# Verifica se há pacotes desatualizados
+if npm outdated | grep -q '.'
+then
+  echo "Pacotes desatualizados encontrados. Instalando dependências..."
+  npm install
+else
+  echo "Todos os pacotes estão atualizados."
+fi
+
+# Faz o git pull com rebase
 git pull --rebase
-npm ci
+
+# Executa o build
 npm run build
+
+# Reinicia a aplicação usando o pm2
 pm2 restart insiderblue || pm2 start npm --name "insiderblue" -- start
