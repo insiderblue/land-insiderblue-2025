bb202ea9b4c52719a266bb56fa43c6447f675724
Gustavo <whoisgustavolima@gmail.com>
2025-03-08 13:58:59 -0300

    lp-insiderblue-2025

--- src/sections/Faq.tsx
+++ src/sections/Faq.tsx
@@ -29,22 +29,22 @@ export default function Faq() {
         {
             question: "Por que tenho que pagar uma mensalidade?",
             answer:
-            "Para manter um site online é necessário contratar uma hospedagem e um domínio...",
+            "Para manter um site online é necessário contratar uma hospedagem e um domínio. A hospedagem normalmente é contratada anualmente, sendo a opção mensal geralmente mais cara. Já o domínio só pode ser contratado anualmente. Para que você não precise se preocupar com isso, a Insiderblue fornece esse serviço mediante uma assinatura mensal acessível. Também está incluso na assinatura a criação de contas de e-mail com 1GB de armazenamento cada, através da plataforma Titan. Todas as configurações são realizadas pelo nosso site, para que você não precise se preocupar com questões técnicas.",
         },
         {
             question: "Existe algum período de fidelidade?",
             answer:
-            "Apesar da hospedagem e o domínio serem normalmente contratados em planos anuais...",
+            "Apesar da hospedagem e o domínio serem normalmente contratados em planos anuais, não trabalhamos com períodos de fidelidade. Portanto, mesmo que registremos o seu domínio por 12 meses, você pode cancelar sua assinatura quando quiser, pagando apenas pelo mês em que o site esteve no ar.",
         },
         {
             question: "Posso fazer o upgrade do meu site depois de pronto?",
             answer:
-            "Você pode realizar o upgrade do seu site depois de pronto...",
+            "Você pode realizar o upgrade do seu site depois de pronto. Nessa hipótese será cobrada a diferença do valor referente ao modelo escolhido inicialmente e o novo modelo, conforme os valores vigentes.",
         },
         {
             question: "Posso mudar o endereço do meu site?",
             answer:
-            "Para que o endereço do site seja alterado será necessário contratar um novo domínio...",
+            "Para que o endereço do site seja alterado será necessário contratar um novo domínio. Será cobrada uma taxa de R$ 80,00 para toda alteração de endereço solicitada. Quando a troca é realizada, o endereço antigo é desativado.",
         },
     ];
 
