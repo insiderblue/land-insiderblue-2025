fa1768f9b74d7166ae80902c6986019c5eb5eaeb
Gustavo <whoisgustavolima@gmail.com>
2025-03-13 17:05:21 -0300

    land-insiderblue-2025

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -70,9 +70,9 @@ const menuQuery = `{
 
 const menu = ((await performRequest(menuQuery)) as { allMenus: Menu[] }).allMenus;
 
-export default async function Hero() { 
-     
-    return (  
+export default async function Hero() {
+ 
+    return (
         <Section id="hero" variant="odd" className="xl:h-screen w-full">
             <video src={hero.video.url} autoPlay playsInline muted loop className="object-cover w-full h-full absolute"></video>
             <div className="absolute inset-0 bg-black opacity-50 z-0"></div>
@@ -84,26 +84,26 @@ export default async function Hero() {
                         alt={layout.title}
                         width={207}
                         height={36}
-                        /> 
-                    </Link> 
+                        />
+                    </Link>
                     <nav className="hidden xl:block">
                         <ul className="flex gap-8 items-center">
-                            {menu.map((item,index) => ( 
+                            {menu.map((item,index) => (
                                 <li key={index}>
                                     <a className="uppercase transition-all ease-in-out duration-300 hover:text-accent" href={item.href}>
                                         {item.title}
-                                    </a>   
-                                </li>   
-                            ))}  
-                        </ul>    
-                    </nav>      
-                    </div>   
+                                    </a>
+                                </li>
+                            ))}
+                        </ul>
+                    </nav>
+                    </div>
                     <div className="flex-1 flex flex-col items-center justify-center text-center">
-                    <h1 className="mb-6 max-w-4xl mx-auto">UP : {hero.title} X</h1>
+                    <h1 className="mb-6 max-w-4xl mx-auto">{hero.title}</h1>
                     <h6 className="mb-10 max-w-3xl mx-auto">{hero.description}</h6>
-                    <Button href={hero.buttonLink}>{hero.buttonTitle}</Button> 
+                    <Button href={hero.buttonLink}>{hero.buttonTitle}</Button>
                 </div>
-            </Container>  
+            </Container>
         </Section>
-    )    
-} 
+    )
+}
